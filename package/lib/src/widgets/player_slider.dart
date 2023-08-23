270a8a7c1109ed31b5e853c4c1db09f69da4661e
João Vitor <42517034+joaojsrbr@users.noreply.github.com>
2023-08-23 14:22:16 -0300

    Update player_slider.dart

--- package/lib/src/widgets/player_slider.dart
+++ package/lib/src/widgets/player_slider.dart
@@ -6,74 +6,51 @@ class PlayerSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _ = MeeduPlayerController.of(context);
-    return Stack(
-      alignment: Alignment.centerLeft,
-      children: [
-        LayoutBuilder(builder: (ctx, constraints) {
-          return RxBuilder(
-            //observables: [_.buffered, _.duration],
-            (__) {
-              return AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                color: Colors.white30,
-                width: constraints.maxWidth * _.bufferedPercent.value,
-                height: 3,
-              );
-            },
-          );
-        }),
-        RxBuilder(
-          //observables: [_.sliderPosition, _.duration],
-          (__) {
-            final double value =
-                _.sliderPosition.value.inMilliseconds.toDouble();
-            final double max = _.duration.value.inMilliseconds.toDouble();
-            // if (value > max || max <= 0) {
-            //   return Container();
-            // }
-            return Container(
-              constraints: const BoxConstraints(
-                maxHeight: 30,
-              ),
-              padding: const EdgeInsets.only(bottom: 8),
-              alignment: Alignment.center,
-              child: SliderTheme(
-                data: SliderThemeData(
-                  trackShape: MSliderTrackShape(),
-                  thumbColor: _.colorTheme,
-                  activeTrackColor: _.colorTheme,
-                  trackHeight: 10,
-                  thumbShape:
-                      const RoundSliderThumbShape(enabledThumbRadius: 4.0),
-                ),
-                child: Slider(
-                  min: 0,
-                  divisions: null,
-                  value: value,
-                  onChangeStart: (v) {
-                    _.onChangedSliderStart();
-                  },
-                  onChangeEnd: (v) {
-                    _.onChangedSliderEnd();
-                    _.seekTo(
-                      Duration(milliseconds: v.floor()),
-                    );
-                  },
-                  label: printDuration(_.sliderPosition.value),
-                  max: max,
-                  onChanged: _.onChangedSlider,
-                ),
-              ),
-            );
-          },
-        )
-      ],
+    final _ = MeeduPlayerScope.controllerOf(context);
+    return RxBuilder(
+      //observables: [_.sliderPosition, _.duration],
+      (__) {
+        final int value = _.sliderPosition.value.inSeconds;
+
+        final double max = _.duration.value.inSeconds.toDouble();
+        if (value > max || max <= 0) return const SizedBox.shrink();
+        return Container(
+          constraints: const BoxConstraints(maxHeight: 30),
+          padding: const EdgeInsets.only(bottom: 8),
+          alignment: Alignment.center,
+          child: SliderTheme(
+            data: SliderThemeData(
+              secondaryActiveTrackColor: Colors.white30,
+              trackShape: _MSliderTrackShape(),
+              thumbColor: _.colorTheme,
+              activeTrackColor: _.colorTheme,
+              showValueIndicator: ShowValueIndicator.always,
+              trackHeight: 10,
+              thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 4.0),
+            ),
+            child: Slider(
+              min: 0,
+              secondaryTrackValue: _.buffered.value.inSeconds.toDouble(),
+              value: value.toDouble(),
+              onChangeStart: (v) {
+                _.onChangedSliderStart();
+              },
+              onChangeEnd: (v) {
+                _.onChangedSliderEnd();
+                _.seekTo(Duration(seconds: v.floor()));
+              },
+              label: printDuration(_.sliderPosition.value),
+              max: max,
+              onChanged: _.onChangedSlider,
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class MSliderTrackShape extends RoundedRectSliderTrackShape {
+class _MSliderTrackShape extends RoundedRectSliderTrackShape {
   @override
   Rect getPreferredRect({
     required RenderBox parentBox,
@@ -82,10 +59,9 @@ class MSliderTrackShape extends RoundedRectSliderTrackShape {
     bool isEnabled = false,
     bool isDiscrete = false,
   }) {
-    const double trackHeight = 1;
+    const double trackHeight = 1.6;
     final double trackLeft = offset.dx;
-    final double trackTop =
-        offset.dy + (parentBox.size.height - trackHeight) / 2 + 4;
+    final double trackTop = offset.dy + (parentBox.size.height - trackHeight) / 2 + 4;
     final double trackWidth = parentBox.size.width;
     return Rect.fromLTWH(trackLeft, trackTop, trackWidth, trackHeight);
   }
